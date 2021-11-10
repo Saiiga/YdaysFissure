9d36b54f9adb838953ba7195e1cbbeaa0559fe2d
Andoradogaris <theolopez47@gmail.com>
2021-11-10 12:28:27 +0100

    ModifEntity

--- Defiance/Assets/Script/Abstract/Item.cs
+++ Defiance/Assets/Script/Abstract/Item.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public abstract class Item : MonoBehaviour
 {
     [SerializeField] protected BoxCollider hitBox;
-    private Material material;
+    private MeshRenderer meshRenderer;
     [SerializeField] protected bool isTakable;
     [SerializeField] protected bool isActionnable = false;
 
     private void Start()
     {
         hitBox = GetComponent<BoxCollider>();
-        material = GetComponent<Material>();
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     public abstract void Action();
