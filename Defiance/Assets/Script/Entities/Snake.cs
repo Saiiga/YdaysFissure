51aa2ec2275b66bf45e7b65cac83aadaafdb091d
Andoradogaris <theolopez47@gmail.com>
2021-12-08 17:20:41 +0100

    LeSerpent

--- Defiance/Assets/Script/Entities/Snake.cs
+++ Defiance/Assets/Script/Entities/Snake.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ public class Snake : MonoBehaviour
 {
     [SerializeField] private List<Transform> body;
 
-    [SerializeField] private ArrayList pos = new ArrayList();
+    [SerializeField] private List<Tuple<Vector3, Quaternion>> pos = new List<Tuple<Vector3, Quaternion>>();
 
     public Transform target;
     public float speed = 3f;
@@ -15,7 +16,7 @@ public class Snake : MonoBehaviour
 
     private Vector3 rotationToAdd;
     private Vector3 positionToAdd;
-    public Transform transformToAdd;
+    public Tuple<Vector3, Quaternion> transformToAdd;
 
     public void Start()
     {
@@ -32,13 +33,20 @@ public class Snake : MonoBehaviour
 
         //Calcul de la prochaine position à rejoindre
         positionToAdd = new Vector3(speed * Time.deltaTime, 0, 0);
-        Debug.Log(positionToAdd);
+        //Debug.Log(positionToAdd);
 
 
-        transformToAdd.position = positionToAdd;
-        transformToAdd.rotation = quaternion;
+        transformToAdd = new Tuple<Vector3, Quaternion> (positionToAdd, quaternion);
+
         //Enregistrement dans la liste
-        pos.Add(transformToAdd);
+        pos.Insert(0, transformToAdd);
+        if(pos.Count >= body.Count)
+        {
+            pos.RemoveAt(body.Count);
+        }
+
+
+
         if (transformToAdd != null)
         {
             //Mouvement de chaque 'body' avec sa pos -1
@@ -51,25 +59,20 @@ public class Snake : MonoBehaviour
     {
         for(int i = 0; pos[i] != null ; i++)
         {
-            //body[i].transform.LookAt(pos[i], Vector3.forward);
+            if(i == 0)
+            {
+                body[i].transform.LookAt(target);
+            }
+            else
+            {
+                body[i].transform.LookAt(body[i - 1]);
+            }
             body[i].transform.Rotate(new Vector3(0, -90, 0), Space.Self);
 
-            if (Vector3.Distance(body[i].transform.position, target.position) > 2f)
+            if (Vector3.Distance(body[i].transform.position, target.position) > 20f)
             {
                 body[i].transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             }
-            
-        }
-
-
-
-
-        transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
-
-        if (Vector3.Distance(transform.position, target.position) > 2f)
-        {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
     }
 
