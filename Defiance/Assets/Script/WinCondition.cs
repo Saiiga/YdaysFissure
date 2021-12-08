896b9659cde1a396b7a8d542c6e88395abe55568
Andoradogaris <theolopez47@gmail.com>
2021-12-08 15:57:14 +0100

    WinCondition

---
+++ Defiance/Assets/Script/WinCondition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private bool hasWin = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasWin = true;
+        }
+    }
+}
